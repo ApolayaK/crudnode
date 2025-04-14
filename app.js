606bb8744e12ae78c34aa9ccc84c815b04cfcde2
Mariana <apolayaschradermariana@gamil.com>
2025-04-14 15:35:08 -0500

    Acceso a datos teminados

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//Motor de plantillas
+app.set('view engine', 'ejs');
+
+//Enrutador - "/" =index
+app.use('/', require('./router'));
+
+app.listen(5000, () => {
+  console.log("Servidor ejecutandose en http://localhost:5000")
+});
