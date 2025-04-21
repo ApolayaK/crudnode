407d36658547e763a6953f558dee2c5aa8f0c9c8
mariana apolaya <apolayaschradermariana@gmail.com>
2025-04-21 15:38:55 -0500

    Finalizado

--- router.js
+++ router.js
@@ -22,8 +22,34 @@ router.get('/create',(req, res)=>{
   res.render('create')
 });
 
+//Es necesario el ID para este proceso
+router.get('/delete/:id', (req,res) =>{
+  const idvehiculo = req.params.id;
+  conexion.query("DELETE FROM vehiculos WHERE id =?", [idvehiculo], (error, results) =>{
+    if (error){
+      throw(error);
+    }else{
+      res.redirect('/');
+    }
+  });
+});
+
+
+//Para editaR , DBEMOS IDENTIFICAR EL REGISTRO
+router.get('/edit/:id', (req,res) =>{
+  conexion.query("SELECT * FROM vehiculos WHERE id = ?", [req.params.id], (error, results) =>{
+    if (error){
+      throw(error);
+    }else{
+      res.render('edit', {vehiculo: results[0]});
+    }
+  });
+});
+
+
 //Acceder a toda la logica
 const crud = require('./controllers/crud');
 router.post('/save', crud.save);
+router.post('/update', crud.update);
 
 module.exports = router;
